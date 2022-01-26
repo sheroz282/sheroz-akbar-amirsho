844a4548dd7b3e1ca45fd24ac9b3d91c3bbf0869
academy-php <zoidovsheroz46@gamil.com>
2022-01-26 15:26:15 +0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+	1) Milk 1 cup
+	2) Honey 4 tsp
+	3) Lemon juice 1 tsp
+	4) Cinnamon or cocoa to taste
+*/
+
+function milkHoney(int $milk, int $honey, int $lemonJuice, int $cinnamonCocoa)
+{
+	echo
+	"$milk Beat milk with honey and lemon juice in a blender or mixer until a slight foam is formed. " .
+	"$honey 4 tsp" .
+	"$lemonJuice 1tsp" .
+	"$cinnamonCocoa Pour the cocktail into glasses. Garnish with cinnamon or cocoa if desired.";
+}
+milkHoney(1, 4, 1, 1);
