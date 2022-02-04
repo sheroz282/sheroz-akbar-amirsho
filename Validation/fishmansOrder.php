c806ead4c9c819ab1c523a95ee2e6aa9f4087672
academy-php <zoidovsheroz46@gamil.com>
2022-02-04 18:19:19 +0500

    Change name function and parameter

--- Validation/fishmansOrder.php
+++ Validation/fishmansOrder.php
@@ -17,38 +17,41 @@ function validateExistingFish(string $accept_order): string
         "Судак",
         "Змеглаво"
     ];
-    foreach ($fish_name as $fishes) {
-        if (is_string($fishes) == $accept_order)
-            return   "200: Fish name type is ok! ";
+
+    if ($fish_name == is_string($accept_order)){
+        return   "200: Fish name type is ok! ";
     }
     return   "422: The type of Fish name is incorrectly! ";
 }
 
 function validateMin($value, $min_value): string
 {
-    if(is_int($value) >= is_int($min_value)){
+    if(is_int($value) >= $min_value){
         return "Min number of characters are ok! ";
+    }else{
+        return "Very few characters in the order! ";
     }
-    return "Very few characters in the order! ";
 }
 
 function validateMax($value, $max_value): string
 {
-    if(is_int($value) <= is_int($max_value)){
+    if(is_int($value) <= $max_value){
         return "Max number of characters are ok! ";
+    }else{
+        return "A lot of characters in the order! ";
     }
-    return "A lot of characters in the order! ";
 }
 
-$value  =  readline("Input the name of fish, please: ");
-$min_value  =  readline("Enter number of fish, minimum 1 please: ");
-$max_value = readline ("Enter number of fish, maximum 3 please: ");
+$max_value = readline ("Enter number of fish, please: ");
 $accept_order = readline("Input the name of fish, please: ");
-function orderToFisherman($accept_order, $value, $min_value, $max_value): string
+#[Pure] function orderToFisherman($accept_order, $value, $min_value, $max_value): string
 {
     $validate_string  = validateExistingFish($accept_order)   . "<br>";
     $validate_min     = validateMin($value, $min_value) . "<br>";
     $validate_max     = validateMax($value, $max_value) . "<br>";
     return $validate_string . $validate_min . $validate_max ;
 }
-print orderToFisherman("$accept_order", "$value", "$min_value", "$max_value");
+echo orderToFisherman("Сазан", "1", "2", "3");
+
+
+
