448747cd8c7ca19dfe1adc7c964e9da7903ef697
Akbar <mubosha.97@gmail.com>
2022-02-05 00:35:13 +0500

    Improved due to comments

--- Validation/coordinates.php
+++ Validation/coordinates.php
@@ -3,7 +3,7 @@
 function validateMax($value, $max_value = 50)
 {
     if ($value >= $max_value) {
-        return "ERROR 422! You've inserted wrong coordinates - exceeds maximum!";
+        return "ERROR 422! You inserted wrong coordinates - over maximum!";
     }
     return "Success! Max is not exceeded.";
 }
@@ -11,25 +11,45 @@ function validateMax($value, $max_value = 50)
 function validateMin($value, $min_value = 20)
 {
     if ($value <= $min_value) {
-        return "ERROR 422! You inserted wrong coordinates - surpass minimum!";
+        return "ERROR 422! You inserted wrong coordinates - under minimum!";
     }
     return "Success! Min is not surpassed.";
 }
 
 function validateType($value)
 {
-    if (!floatval($value)) {
+    if (!filter_var($value, FILTER_VALIDATE_FLOAT)) {
         return "ERROR: Type is not float!";
     }
     return "Success: Type is float!";
 }
 
+function validateScope($value, $point_a, $point_b)
+{
+    if ($value <= $point_a && $value >= $point_b){
+        return "ERROR! You are out of scope!";
+    }
 
-function validateCoordinates($latitude, $longitude){
-    validateType($latitude);
-    validateType($longitude);
-    validateMin($latitude);
-    validateMax($latitude);
-    validateMin($longitude);
-    validateMax($longitude);
+    return "Got your spot! We will pick you up!";
 }
+
+
+function validateCoordinates($latitude, $longitude)
+{
+    $validation_message = "";
+    $validation_message .= validateType($latitude) . " - latitude\n";
+    $validation_message .= validateType($longitude) . " - longitude\n";
+    $validation_message .= validateMin($latitude) . " - latitude\n"; // you can insert 2nd param if needed
+    $validation_message .= validateMax($latitude) . " - latitude\n"; // you can insert 2nd param if needed
+    $validation_message .= validateMin($longitude) . " - longitude\n"; // you can insert 2nd param if needed
+    $validation_message .= validateMax($longitude) . " - longitude\n"; // you can insert 2nd param if needed
+    $validation_message .= validateScope($longitude, 0, 90) . " - longitude\n";
+    $validation_message .= validateScope($latitude, 0, 180) . " - latitude\n";
+    return $validation_message;
+}
+
+$my_latitude = readline("Введите координаты широты:");
+$my_longitude = readline("Введите координаты долготы:");
+
+$result = validateCoordinates($my_latitude, $my_longitude);
+print($result);
