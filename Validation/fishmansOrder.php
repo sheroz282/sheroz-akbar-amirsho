20b3c7e0cfe6629396c6118a618fe422da98b6db
academy-php <zoidovsheroz46@gamil.com>
2022-02-05 12:01:05 +0500

    Change hw-8 on type and work ability

--- Validation/fishmansOrder.php
+++ Validation/fishmansOrder.php
@@ -10,48 +10,68 @@ INPUT: Имя рыбы, количество рыбы
 находится для в наборе уже существующих рыб в океане (создаете отдельный массив доступных рыб)
 */
 
-function validateExistingFish(string $accept_order): string
+use JetBrains\PhpStorm\Pure;
+
+function validateString($fish_name): string
 {
-    $fish_name = [
+    if(!is_string($fish_name)){
+        return   "422: The type of Fish name is incorrectly! ";
+    }else{
+        return   "200: Fish name type is ok! ";
+    }
+}
+
+function validateInt($fish_count): string
+{
+    if(is_int($fish_count) == 0){
+        return   "422: The type of Fish name is incorrectly! ";
+    }else{
+        return   "200: Fish name type is ok! ";
+    }
+}
+
+function validateMin(string $fish_name, $min_value): string
+{
+    if(strlen($fish_name) <= $min_value){
+        return "Very few characters in the order! ";
+    }else{
+        return "Min number of characters are ok! ";
+    }
+}
+
+function validateMax(string $fish_name, $max_value): string
+{
+    if(strlen($fish_name) >= $max_value){
+        return "A lot of characters in the order! ";
+    }else{
+        return "Max number of characters are ok! ";
+    }
+}
+
+function validateExistingFish(string $fish_name, array $existing_fish): string
+{
+    if(!in_array($fish_name, $existing_fish)){
+        return "There is no such fish on the list";
+    }else{
+        return "There is such a fish";
+    }
+}
+
+$fish_count = readline ("Enter number of fish, please: ");
+$fish_name = readline("Input the name of fish, please: ");
+#[Pure] function orderToFisherman(string $fish_name, string $fish_count): string
+{
+    $existing_fish = [
         "Сазан",
         "Судак",
         "Змеглаво"
     ];
 
-    if ($fish_name == is_string($accept_order)){
-        return   "200: Fish name type is ok! ";
-    }
-    return   "422: The type of Fish name is incorrectly! ";
+    $validate_string  = validateString($fish_name)   . "<br>";
+    $validate_int     = validateInt($fish_count) . "<br>";
+    $validate_min = validateMin($fish_name, 1) . "<br>";
+    $validate_max     = validateMax($fish_name, 3) . "<br>";
+    $existing_fish     = validateExistingFish($fish_name, $existing_fish) . "<br>";
+    return $validate_string . $validate_int . $validate_min . $validate_max . $existing_fish;
 }
-
-function validateMin($value, $min_value): string
-{
-    if(is_int($value) >= $min_value){
-        return "Min number of characters are ok! ";
-    }else{
-        return "Very few characters in the order! ";
-    }
-}
-
-function validateMax($value, $max_value): string
-{
-    if(is_int($value) <= $max_value){
-        return "Max number of characters are ok! ";
-    }else{
-        return "A lot of characters in the order! ";
-    }
-}
-
-$max_value = readline ("Enter number of fish, please: ");
-$accept_order = readline("Input the name of fish, please: ");
-#[Pure] function orderToFisherman($accept_order, $value, $min_value, $max_value): string
-{
-    $validate_string  = validateExistingFish($accept_order)   . "<br>";
-    $validate_min     = validateMin($value, $min_value) . "<br>";
-    $validate_max     = validateMax($value, $max_value) . "<br>";
-    return $validate_string . $validate_min . $validate_max ;
-}
-echo orderToFisherman("Сазан", "1", "2", "3");
-
-
-
+echo orderToFisherman($fish_name, $fish_count);
