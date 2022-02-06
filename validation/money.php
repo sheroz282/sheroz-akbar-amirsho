982c70f2279241dbae775d5b72fe2cfa5ede29f9
Amirsho <amirshorakhimov0017@gmail.com>
2022-02-06 12:58:01 +0500

    mixsed

--- validation/money.php
+++ validation/money.php
@@ -1,41 +1,44 @@
 <?php
-function validateInt($money_in_cents)
+function validateInt($value)
 {
-    if (is_int($money_in_cents))
-    {
-        return "Error! Incorrect data";
+    if (($value = intval($value)) && ($value > 0)) {
+        return "OK! type integer: $value ";
     }
-
-    return "OK! 200. ";
+    return "Error! Incorrect data of type : $value ";
 }
 
-function validateMin($money_in_cents, $min_value)
+function validateMin($value, $min)
 {
-    if ($money_in_cents <= $min_value)
-    {
-        return "ERROR! This amount of money is not enough";
+    if (mb_strlen($value) >= $min) {
+        return "OK! Correct value of min: $value ";
     }
-    return "OK! This amount of money in the range";
+    return "ERROR! Incorrect min: $value ";
 }
 
-function validateMax($money_in_cents, $max_value)
+function validateMax($value, $max)
 {
-    if ($money_in_cents >= $max_value) {
-        return "ERROR! This amount of money is a lot";
+    if (mb_strlen($value) <= $max) {
+        return "OK! Correct value of max: $value ";
     }
-    return "OK! This amount of money in the range";
+    return "ERROR! Incorrect value of max: $value ";
 }
 
 function validateTransfer($money_in_cents, $sender_account_number, $recipient_account_number)
 {
-    $validate_message = "";
-    $validate_message .= validateInt($money_in_cents);
-    $validate_message .= validateMin($money_in_cents, 10000);
-    $validate_message .= validateMax($money_in_cents, 100000);
-    return "$validate_message";
+    $validate_money = validateInt($money_in_cents);
+    $validate_sender_account_num = validateInt($sender_account_number);
+    $validate_recipient_account_num = validateInt($recipient_account_number);
+    $validate_money_min = validateMin($money_in_cents, 2);
+    $validate_sender_number_min = validateMin($sender_account_number, 9);
+    $validate_recipient_number_min = validateMin($recipient_account_number, 9);
+    $validate_money_max = validateMax($money_in_cents, 10);
+    $validate_sender_number_max = validateMax($sender_account_number, 12);
+    $validate_recipient_number_max = validateMax($recipient_account_number, 12);
+    return $validate_money . $validate_sender_account_num . $validate_recipient_account_num . $validate_money_min . $validate_sender_number_min . 
+           $validate_recipient_number_min . $validate_money_max . $validate_sender_number_max . $validate_recipient_number_max; 
 }
 
-$money_in_cents = 15000;
-$sender_account_number = 944445222;
-$recipient_account_number = 933007337;
-validateTransfer($money_in_cents, $sender_account_number, $recipient_account_number);
+$money_in_cents = readline('Enter transfer amount: ');
+$sender_account_number = readline('Enter sender account number: ');
+$recipient_account_number = readline('Enter recipient account number: ');
+echo validateTransfer($money_in_cents, $sender_account_number, $recipient_account_number);
