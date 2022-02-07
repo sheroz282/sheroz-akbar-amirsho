f4ebc6c267110db9d44e076838d3ca2376d897dc
academy-php <zoidovsheroz46@gamil.com>
2022-02-07 15:05:04 +0500

    refactoring add fishmansOrder.php

--- Validation/fishmansOrder.php
+++ Validation/fishmansOrder.php
@@ -10,8 +10,6 @@ INPUT: Имя рыбы, количество рыбы
 находится для в наборе уже существующих рыб в океане (создаете отдельный массив доступных рыб)
 */
 
-use JetBrains\PhpStorm\Pure;
-
 function validateString($fish_name): string
 {
     if(!is_string($fish_name)){
@@ -30,7 +28,7 @@ function validateInt($fish_count): string
     }
 }
 
-function validateMin(string $fish_name, $min_value): string
+function validateMin($fish_name, $min_value): string
 {
     if(strlen($fish_name) <= $min_value){
         return "Very few characters in the order! ";
@@ -39,39 +37,46 @@ function validateMin(string $fish_name, $min_value): string
     }
 }
 
-function validateMax(string $fish_name, $max_value): string
+function validateMax($fish_name, $max_value): string
 {
-    if(strlen($fish_name) >= $max_value){
+    if (strlen($fish_name) >= $max_value) {
         return "A lot of characters in the order! ";
-    }else{
+    } else {
         return "Max number of characters are ok! ";
     }
 }
 
-function validateExistingFish(string $fish_name, array $existing_fish): string
+function validateExistingFish($fish_name, $existing_fish): string
 {
-    if(!in_array($fish_name, $existing_fish)){
+    if(!in_array($fish_name, (array)$existing_fish)){
         return "There is no such fish on the list";
     }else{
         return "There is such a fish";
     }
 }
 
+function validate($rules, $request, $existing_fish): array
+{
+    $existing_fish = [];
+    $separate_rules = explode('|', $rules);
+    foreach ($separate_rules as $rule){
+        $rule_with_parameters = explode(':', $rule);
+        array_push($existing_fish,
+            call_user_func_array('validate' . ucfirst($rule_with_parameters[0]),[$request, $separate_rules[0]]));
+    }
+    return $existing_fish;
+}
+
+function index($request): string
+{
+    $existing_fish = ["Сазан", "Судак", "Змеглаво"];
+    $rules = 'string|int|min:1|max:3|existingFish:$existing_fish';
+    $messages = validate($rules, $request, $existing_fish);
+    return count($messages)
+        ? implode(',\n', $messages)
+        : "No validation message";
+}
 $fish_count = readline ("Enter number of fish, please: ");
 $fish_name = readline("Input the name of fish, please: ");
-#[Pure] function orderToFisherman(string $fish_name, string $fish_count): string
-{
-    $existing_fish = [
-        "Сазан",
-        "Судак",
-        "Змеглаво"
-    ];
-
-    $validate_string  = validateString($fish_name)   . "<br>";
-    $validate_int     = validateInt($fish_count) . "<br>";
-    $validate_min = validateMin($fish_name, 1) . "<br>";
-    $validate_max     = validateMax($fish_name, 3) . "<br>";
-    $existing_fish     = validateExistingFish($fish_name, $existing_fish) . "<br>";
-    return $validate_string . $validate_int . $validate_min . $validate_max . $existing_fish;
-}
-echo orderToFisherman($fish_name, $fish_count);
+echo index($fish_count);
+echo index($fish_name);
